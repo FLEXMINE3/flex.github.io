d84acf86171d3244bc4ce1be4fb16f87d93b8823
FLEXMINE3 <103375881+FLEXMINE3@users.noreply.github.com>
2022-04-10 18:46:43 +0200

    Add files via upload

---
+++ tonk/module/inc-menu.php
@@ -0,0 +1,59 @@
+<div class='menu-con-release'>
+	
+</div>
+<div class='menu-con-message fade-out-onload'>
+	<div class='menu-tx1-message toggle-message fade-out-onload'>
+	</div>
+</div>
+<div class="window-con-theme">
+	<div class="window-bg-theme">
+	</div>
+	<div class="theme-con-spinner">
+		<div class="theme-ico-spinner">
+		</div>
+	</div>
+	
+	<div class="wavebar-con-container-theme">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+</div>
+<div class="theme-btn-close toggle-theme">
+</div>
+
+<div class="menu-btn-backtotop">
+</div>
+<div class='menu-con-container'>
+	<div class="menu-con-backdrop darkmode-menu">
+	</div>
+	<div class="menu-con-outer">
+		<div class="menu-con-inner">
+			<div class="mobile-menu-btn-open toggle-mobilemenu">
+			</div>
+			<div class="mobile-menu-btn-darkmode toggle-darkmode">
+			</div>
+			<a href='/методы'>
+			<div class="menu-btn-select">
+				<span>Методы противодействия финансовому мошенничеству</span>
+			</div>
+			</a>
+			<div class="support-con-menu">
+			</div>
+		</div>
+	</div>
+</div>
+<div class='mobile-menu-con-container popup-mobilemenu' style="display:none;">
+	<div class="menu-con-backdrop-2">
+	</div>
+	<div class='mobile-menu-con-scroll'>
+		<div class="mobile-menu-con-outter">
+			<div class="mobile-menu-con-inner" style="overflow: hidden !important;">
+				
+			</div>
+		</div>
+	</div>
+</div>
---
+++ tonk/module/sys-php.php
@@ -0,0 +1,5 @@
+<?php
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/lib/module/сайт3.php";
+   include_once($path);
+?>
