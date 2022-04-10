6cc0cd301a86d385f9f6a2035324b440dcf19020
FLEXMINE3 <103375881+FLEXMINE3@users.noreply.github.com>
2022-04-10 18:39:46 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,198 @@
+<!doctype html>
+<html lang="ru-рус">
+<head>
+<title>Виды мошенничества и способы противодействия</title>
+<meta charset="utf-8">
+<meta name="google-site-verification" content="cO1o6sx54cvKxhbnYsABWtl4sYFj9uVKV0DxLKZkWv8"/>
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:image" content="https://вмсп.net/img/meta/mobile/1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://вмсп.zzz.com.ua" />
+<meta property="og:locale" content="ru_рус"/>
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="вмсп" />
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+<script data-cfasync="false" src='/lib/js/carousel.js?v=1'></script>
+</head>
+<body>
+<img src="/img/graphics/landing/hero-main.png" width="1" height="1" alt="preload"/>
+<img src="/img/videos/1.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3.jpg" width="1" height="1" alt="Preload"/>
+<?php include 'lib/module/sys-php.php';?>
+<?php include 'lib/module/inc-video-popout.php'?>
+<?php
+	preloadVideo(
+	'1',
+	'60lq9pXzDic',
+	'1'
+	);
+	preloadVideo(
+	'2',
+	'MXHfGzEDz-I',
+	'2'
+	);
+	preloadVideo(
+	'3',
+	'PU3A755xcDQ',
+	'3'
+	);
+?>
+<div class="page-con-content landing-con-override">
+	<div class="wavebar-con-container-master">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-main">
+		<div id="object-particles">
+		</div>
+		<div class="content-con-backdrop darkmode-header">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class='landing-con-left content-expand '>
+					<div class="landing-con-container scale-content-txt-1 fade-in-onload">
+						<div class='landing-ico-logo'>
+						</div>
+						<div class='landing-tx1-heading pulsate'>
+							<h1>Финансовое<br>
+							 Мошенничество</h1>
+						</div>
+						<div class='landing-tx2-heading'>
+							<p>
+								Списание денег со счета без ведома владельца, кража паролей и ПИН-кодов, легкий заработок в интернете и вклады под невероятные проценты, онлайн-казино — все это виды финансового мошенничества. Преступники будут спекулировать на ваших чувствах, обещать золотые горы, маскироваться под сотрудников банков или государственные организации, чтобы выманить деньги. 
+							</p>
+							<div class='landing-btn-container'>
+									<div class="build-ico-button">
+										<div class="build-ico-os">
+										</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='landing-con-right content-remove '>
+					<div class='landing-img-hero' style="background: url(/img/graphics/landing/1сайт.png) no-repeat center; background-size: contain;">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-search darkmode-slimbar">
+		<div class="search-ovr-container">
+		</div>
+		<div class='search-con-container'>
+			<div class="search-con-outer">
+				<div class="search-con-inner">
+				</div>
+			</div>
+		</div>
+	</div>
+				<div class="video-tx1-heading darkmode-txt">
+			</div>
+	<div class="landing-con-discord darkmode-discord">
+		
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="discord-con-container">
+					<div class="discord-con-logo">
+						<div class='discord-img-logo' style="background: url(/img/graphics/landing/тм.png) no-repeat center; background-size: contain;">
+						</div>
+					</div>
+					<div class="discord-con-mantra">
+						<div class="discord-wrp-mantra scale-content-txt-1">
+							<div class="discord-tx1-mantra darkmode-txt">
+								<h2>Телефонные мошенники</h2>
+							</div>
+							<div class="discord-tx2-mantra darkmode-txt">
+								<p>
+									Если вам звонят из банка и просят сообщить персональные данные или информацию о карте/счете — будьте бдительны, это могут быть мошенники! Злоумышленники используют специальные технологии и на экране вашего телефона высвечивается официальный номер банка. Они могут обратиться к вам по имени-отчеству и попросить секретные сведения о карте или счете. Например, чтобы остановить подозрительную операцию.
+									</p> 
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<svg class="discord-con-animate" preserveaspectratio="none" width="1920" height="100%" viewbox="0 0 1920 330">
+		<path class="discord-svg-animate" fill="#fff" fill-opacity="0.1" d="M140.881198,194.260295 C257.600568,129.32862 342.939626,119.84993 418.009939,203.154617 C493.080251,286.459305 545.728689,70.9046172 636.439626,63.9593047 C727.150564,57.0139922 768.99822,139.670242 858.802907,119.431961 C948.607595,99.1936797 1071.91228,-32.9977266 1243.91228,7.75227342 C1415.91228,48.5022734 1404.10369,208.584305 1508.27166,178.709305 C1612.43963,148.834305 1633.73291,79.913472 1711.63588,98.8569055 C1776.28676,114.577866 1819.96778,221.391836 1889.37253,185.808108 C2017.32661,120.206212 2004.01952,336.769569 2004.01952,336.769569 L271.635881,337 L-149.063338,337 C-149.063338,337 -245.850307,175.637635 -58.0633382,228.867188 C33.8652851,254.92501 64.1722713,236.933925 140.881198,194.260295 Z"></path>
+		</svg>
+	</div>
+	<div class="landing-con-adsense">
+		<div class='adsense-tx1-description darkmode-txt'>
+		</div>
+		<div class="adsense-con-container darkmode-slimbar div-css-board-ad">
+			<div class="adsense-con-frame">
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-contribute darkmode-github">
+		<div class="github-img-backdrop" style="background: url(/img/graphics/landing/мм.png) no-repeat center; background-size: contain;">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="github-con-container">
+					<div class="github-con-logo">
+						<div class='github-img-logo' style="background: url(/img/graphics/landing/бк.png) no-repeat center; background-size: contain;">
+						</div>
+					</div>
+					<div class="github-con-mantra">
+						<div class="github-wrp-mantra scale-content-txt-1 ">
+							<div class="github-tx1-mantra darkmode-txt">
+								<h2>Мошенничество с банковскими картами</h2>
+							</div>
+							<div class="github-tx2-mantra darkmode-txt">
+								<p>
+									 Чтобы использовать вашу карту в своих целях, мошенникам нужно узнать ее номер, имя владельца, срок действия, номер CVC или CVV. Они могут установить скиммер на банкомат (специальное устройство, которое накладывают на приемник карты в банкомате) и видеокамеру над клавиатурой.
+
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-adsense">
+		<div class='adsense-tx1-description darkmode-txt'>
+		</div>
+		<div class="adsense-con-container darkmode-slimbar div-css-board-ad">
+		</div>
+	</div>
+	<div class="landing-con-patreon darkmode-feature">
+		<div class="patreon-img-backdrop">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="patreon-img-logo" style="background: url(/img/graphics/landing/км.png) no-repeat center; background-size: contain;">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra">
+								<h2>Кибермошенничество. Как не попасться?!</h2>
+								<div style="background: #ff424d">
+								</div>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+									 Кибермошенники придумывают разные способы обмана. На пример, вам приходит СМС или письмо «от банка» со ссылкой, просьбой перезвонить или уведомление о крупном выигрыше. Или звонят «из банка» и просят отправить личные данные. Или пишут в социальных сетях от имени родственников или друзей, которые попали в беду, и просят перевести деньги на неизвестных счет.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
---
+++ методы.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="ru-рус">
+<head>
+<title>Методы противодействия финансовому мошенничеству</title>
+<meta charset="utf-8">
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="Финансовое мошенничество" />
+<meta property="og:image" content="https://пфм.net/img/meta/mobile/1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://пфм.zzz.com.ua" />
+<meta property="og:locale" content="RU_РУС"/>
+<meta property="og:type" content="website" />
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>Противодействие финансовому мошенничеству по телефону</h1>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class="landing-con-panel" style="background: url('/img/graphics/panels/методы.jpg') no-repeat center;">
+				<div class='landing-ovr-panel'>
+					<div class='landing-tx1-panel'>
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Телефонное мошенничество</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									В 2022 году наиболее распространенным видом мошенничества стало мошенничество в сфере информационно-телекоммуникационных технологий или компьютерной информации. По данным Генпрокуратуры РФ, на них приходится около 70% всех хищений, совершенных путем обмана или злоупотребления доверием. При этом более 42,7% мошенничеств совершалось с использованием средств мобильной связи. <br>
+									<br>
+									 Действия телефонных мошенников квалифицируются по ст. 159 Уголовного кодекса как мошенничество, то есть умышленные действия, направленные на хищение чужого имущества путем обмана или злоупотребления доверием. Противодействие такому виду мошенничества осуществляется государственными органами не только с помощью регистрации и расследования уголовных преступлений, но и путем информирования граждан о потенциальной опасности.
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/методы/телефон.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-img-container splitter-con-left'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/методы/люди.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Противодействие телефонному мошенничеству</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>Правила, которые помогут не стать жертвой мошенников:
+									1)Не верить телефонным звонкам с неизвестных номеров о том, что ваш родственник, близкий или знакомый совершили, или пострадали в ДТП, стали соучастниками преступления, и за решения вопроса о привлечении их к ответственности необходимы деньги. Задайте звонящему вопросы личного характера, помогающие отличить близкого человека от мошенника. Под любым предлогом прервать контакт с собеседником и перезвонить родным и узнать, все ли у них в порядке.
+									<br>
+									 2)Не разглашать свои персональные данные, такие как фамилия, имя, отчество, паспортные данные, данные банковских карт, счетов, а также защитные коды и пароли, ни под каким предлогом;
+									3)Не передавать деньги не знакомым лицам, не под каким предлогом.
+									4)Не соглашаться на предложение обменять деньги на новые или иностранные купюры. Рассказы о грядущей денежной реформы не правда.
+
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									5)Не доверять СМС-сообщениям, приходящим на телефон, будь то крупный выигрыш, победа в конкурсе или лотереи, особенно в тех случаях, когда для получения выигрыша просят оплатить налог. Необходимо знать, что настоящий розыгрыш призов не должен подразумевать денежные выплаты с Вашей стороны!
+                     			 6)Не перезванивать на номер, с которого пришло СМС-сообщение о том, что банковская карта заблокирована и не отправлять ответных смс-сообщений. Позвоните в банк, выпустивший и обслуживающий карту (телефон банка указан на обороте банковской карты).
+                     			  7) Не отправлять денежные средства на неизвестные адреса, в том числе с целью приобретения вещей в сети Интернет.
+									<br>
+									 Если каким-то образом мошенникам стали известны данные вашей карты, то карту необходимо заблокировать. Чтобы избежать списания денежных средств, также необходимо заблокировать мобильный и интернет банк. Сделать это можно самостоятельно через приложение банка или обратившись в службу поддержки банка по телефону горячей линии. Чтобы продолжить пользоваться услугами банка, карту необходимо будет перевыпустить.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/методы/ласт.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+</body>
+</html>
